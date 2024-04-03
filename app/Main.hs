84d079e658055def5c45127075a0ac3398fdb398
Nate Brown <n.b.nrb99@icloud.com>
2024-04-03 07:32:25 -0500

    made logging optional

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,24 @@
 
 module Main (main) where
 
-import Data.Text.IO (getLine)
+import Control.Spoon (spoon)
+import qualified Data.Text.IO as T
 import Lang (prepare, simp)
+import Logging (forget, writeLog)
 import Parsers (lang)
 import Text.Megaparsec (errorBundlePretty, parse)
-import Prelude hiding (getLine)
-import Logging (writeLog)
+import Prelude
 
 main :: IO ()
 main = do
-    line <- getLine
+    putStrLn "Please provide a lambda-calculus expression:"
+    line <- T.getLine
+    putStrLn "And would you like debug output? (1 for yes, anything else for no)"
+    putStrLn "Be warned, it is long and (mostly) useless"
+    dbg <- getLine
     case parse lang "<stdin>" line of
         Left e -> putStrLn ">:(" >> putStr (errorBundlePretty e)
-        Right ex -> do
-            print ex
-            ex' <- writeLog $ simp ex
-            ex'' <- writeLog $ simp $ prepare ex'
-            print ex''
+        Right ex ->
+            if spoon (read dbg :: Integer) == Just 1
+                then print =<< (writeLog $ simp =<< prepare <$> simp ex)
+                else print $ forget $ simp =<< prepare <$> simp ex
