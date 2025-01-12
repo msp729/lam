4b3647916e33eb16ec59648dab6dce173f2ce882
Nate Brown <nravinbrown@gmail.com>
2025-01-12 00:38:15 -0600

    added haskeline

--- app/Main.hs
+++ app/Main.hs
@@ -2,37 +2,39 @@
 
 module Main (main) where
 
-import Control.Spoon (spoon)
-import Data.Text.IO qualified as T
+import Control.Monad.Trans.Class
+import Data.Maybe
+import Data.Text qualified as T
 import Lang (prepare, simp)
 import Logging (forget, writeLog)
 import Parsers (lang)
+import System.Console.Haskeline
 import Text.Megaparsec (errorBundlePretty, parse)
 import Prelude
 
 main :: IO ()
-main = do
-    putStrLn "Would you like debug output? (1 for yes, anything else for no)"
-    putStrLn "Be warned, it is long and (mostly) useless"
-    dbg <- getLine
-    interp (spoon (read dbg :: Integer) == Just 1)
+main = runInputT defaultSettings app
 
-interp :: Bool -> IO ()
-interp b = do
-    putStrLn "Please provide a λ-calculus expression:"
-    line <- T.getLine
-    if line == ":q"
-        then return ()
-        else
-            if line == ""
-                then interp b
-                else do
-                    case parse lang "<stdin>" line of
-                        Left e -> putStrLn "invalid" >> putStr (errorBundlePretty e)
-                        Right ex ->
-                            if b
-                                then
-                                    print
-                                        =<< (writeLog $ simp =<< prepare <$> simp ex)
-                                else print $ forget $ simp =<< prepare <$> simp ex
-                    interp b
+app :: InputT IO ()
+app = do
+    outputStrLn "Would you like debug output? (1 for yes, anything else for no)"
+    outputStrLn "Be warned, it is long and (mostly) useless"
+    dbg <- getInputLine "? "
+    case dbg of
+        Nothing -> return ()
+        Just x -> interp (x == "1")
+
+interp :: Bool -> InputT IO ()
+interp dbg = do
+    outputStrLn "Please provide a λ-calculus expression:"
+    line <- (T.pack . fromMaybe ":q") <$> getInputLine "> "
+    handle line dbg
+
+handle :: T.Text -> Bool -> InputT IO ()
+handle ":q" _ = return ()
+handle "" dbg = interp dbg
+handle l dbg = do
+    case parse lang "<stdin>" l of
+        Left e -> outputStrLn "invalid" >> outputStr (errorBundlePretty e)
+        Right ex -> (outputStrLn . show) =<< ((if dbg then lift . writeLog else pure . forget) $ simp =<< prepare <$> simp ex)
+    interp dbg
